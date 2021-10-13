050b3e8cd7af5b9f44f9c7f3579909e7da78662e
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-10-13 18:58:01 +0530

    Create Practical-07_Task-3_V1.cpp

---
+++ Practical-07/Practical-07_Task-3_V1.cpp
@@ -0,0 +1,34 @@
+//Task 1 : Making resultant matrix by reversing (without restoration)
+/*
+      1  2  3          9  8  7
+      4  5  6     =>   6  5  4
+      7  8  9          3  2  1
+*/ 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int arr[3][3],j,n,i;
+  cout<<"Enter the value of n : ";
+  cin>>n;
+  cout<<"Enter 2D Matrix"<<"\n";
+  for(i=0;i<n;i++) 
+  {
+    for(j=0;j<n;j++)
+    {
+      cin>>arr[i][j];
+    }
+  }
+  cout<<"\nResultant matrix : \n";
+  for(i=n-1;i>=0;i--) 
+  {
+    for(j=n-1;j>=0;j--)
+    {
+      cout<<arr[i][j]<<" ";
+    }
+    cout<<"\n";
+  }
+  
+  return 0;
+}
