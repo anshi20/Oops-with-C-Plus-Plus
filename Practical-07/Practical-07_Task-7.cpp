f794e027567f1870fef72d1eefaac9f6b2e58bc8
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-10-14 10:32:30 +0530

    Create Practical-07_Task-7.cpp

---
+++ Practical-07/Practical-07_Task-7.cpp
@@ -0,0 +1,30 @@
+/*
+
+    Task 7: Print the following pattern : 
+             
+            if n = 4              *
+                                * *
+                              * * *
+                            * * * *
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{   
+   int i,j,n;
+   cout<<"Enter the value of n :";
+   cin >> n;
+   for(i=0;i<n;i++)
+   { 
+     for(j=0;j<n;j++)
+      {
+          int sum = i + j;
+          if(sum>=n-1)
+             cout<<"*";
+          else
+             cout<<" ";
+      }cout<<"\n";
+   }
+   return 0;  
+}
