800169e67ca6acfc36b26f2a0a22487e7f8a7534
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-10-14 10:52:00 +0530

    Add files via upload

---
+++ Practical-07/Practical-07_Task-4_V1.cpp
@@ -0,0 +1,28 @@
+/*
+
+    Task 4: 
+              INPUT                 OUTPUT
+             
+              1 2 3                  1 1 1
+              4 5 6                  2 2 2
+              7 8 9                  3 3 3
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   int i,j,n;
+   cout<<"Enter the value of n : ";
+   cin>>n;
+   for(i=0;i<n;i++)   
+   {
+     for(j=0;j<n;j++)
+      {
+           cout<<i+1;
+      }
+     cout<<"\n";
+   }
+  
+   return 0;
+}
