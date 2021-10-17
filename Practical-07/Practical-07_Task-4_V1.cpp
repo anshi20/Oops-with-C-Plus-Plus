c93d01c02dec8bfd9a1bd72d590c9c18e4ceeda0
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-10-18 00:03:50 +0530

    Update Practical-07_Task-4_V1.cpp

--- Practical-07/Practical-07_Task-4_V1.cpp
+++ Practical-07/Practical-07_Task-4_V1.cpp
@@ -12,9 +12,16 @@
 using namespace std;
 int main()
 {
-   int i,j,n;
+   int i,j,n,arr[3][3];
    cout<<"Enter the value of n : ";
    cin>>n;
+   for(i=0;i<n;i++)   
+   {
+     for(j=0;j<n;j++)
+      {
+           cin>>arr[i][j];
+      }
+   }
    for(i=0;i<n;i++)   
    {
      for(j=0;j<n;j++)
