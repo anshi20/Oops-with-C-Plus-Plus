4594f555c411f8a522fa8c8924dc3e67c052e0cf
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-10-18 10:37:34 +0530

    Add files via upload

---
+++ Practical-08/Practical-08_Task-2_V1.cpp
@@ -0,0 +1,21 @@
+/*
+
+   Task 2 : Version 1
+  
+*/
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   int arr[]={10,20,30};
+   int *p;
+   p=arr;
+   cout<<*p<<endl;
+   p++;
+   cout<<*p;
+   return 0;
+}
+
