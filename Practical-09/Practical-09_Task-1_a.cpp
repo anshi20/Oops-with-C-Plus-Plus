8aceb272c38845aecd67c7784e41fea33ca0f8e3
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-11-01 10:27:33 +0530

    Update and rename Practical-09_Task-1.cpp to Practical-09_Task-1_a.cpp

---
+++ Practical-09/Practical-09_Task-1_a.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+class Student {
+   private:
+	char name[30];
+	char phone_no[13];
+    char address[30];
+   public:
+   	void read()
+   	{
+   	  cout<<"\nName : ";
+	  gets(name);
+	  cout<<"Phone No. : ";
+	  gets(phone_no);
+	  cout<<"Address : ";
+	  gets(address);
+	}
+	void display()
+    {
+      cout<<"\nName : "<<name;
+	  cout<<"\nPhone No. : "<<phone_no;
+	  cout<<"\nAddress : "<<address;
+    }
+};
+int main()
+{
+	Student s1, s2;
+	s1.read();
+    s2.read();
+    s1.display();
+    cout<<endl;
+    s2.display();
+    return 0;
+}
