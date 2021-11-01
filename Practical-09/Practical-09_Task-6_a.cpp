01e70d217b791042ad0b60c1f249d8b862acc159
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-11-01 10:14:01 +0530

    Create Practical-09_Task-6_a.cpp

---
+++ Practical-09/Practical-09_Task-6_a.cpp
@@ -0,0 +1,18 @@
+/*
+          9.6 Task:              Extra Questions:
+          
+          WAP which would perform these tasks of your data:
+          a. Come to next line
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"We can come to next line in two ways : ";
+   cout<<endl;   \\endl manipulator
+   cout<<"First is \'endl\' ";
+   cout<<"\n";   \\escape sequence
+   cout<<"Second is escape sequence \'\\n\' ";
+   return 0;
+}
