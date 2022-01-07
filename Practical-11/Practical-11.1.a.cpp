ed112b42765c6ffd407596c3e279f05680c55331
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2022-01-07 12:33:36 +0530

    Update and rename Practical-11_Task-1_v1.cpp to Practical-11.1.a.cpp

---
+++ Practical-11/Practical-11.1.a.cpp
@@ -0,0 +1,25 @@
+// Call Parent class method in child class function without creating an object of parent class. 
+
+#include<iostream>
+using namespace std;
+class A{
+    public:
+       void displayA()
+       {
+        cout << " Parent Class Method " << endl;
+       }
+};
+class B: public A{
+    public:
+       void displayB()
+       {
+          cout << "Calling Parent class method in child class function" << endl;
+          displayA();
+       }  
+};
+int main()
+{
+    B obj;  //Simple Oops concept
+    obj.display();     //Simple Oops concept
+    return 0;
+}
