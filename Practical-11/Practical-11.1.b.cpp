c22298608a4a1be45ba5383ffa592df3be288d3f
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2022-01-07 13:08:38 +0530

    Create Practical-11.1.b.cpp

---
+++ Practical-11/Practical-11.1.b.cpp
@@ -0,0 +1,25 @@
+// Call Parent class method in main method by Child class object.
+
+#include <iostream>
+using namespace std;
+class A{
+public:
+    void displayA()
+    {
+        cout << "Class A Method Called " << endl;
+    }
+};
+class B : public A
+{
+
+public:
+    void displayB()
+    {
+        cout << "calling Class A method from class B method" << endl;
+        displayA();
+    }
+};
+int main(){
+    B obj;
+    obj.displayA();
+}
